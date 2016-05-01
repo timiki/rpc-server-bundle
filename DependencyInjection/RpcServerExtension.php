7632696d0a8485f233722a19c7f60eba1273c937
Nikita Netyosin <nikita@netesin.ru>
2016-05-01 19:57:38 +0800

    Update

--- DependencyInjection/RpcServerExtension.php
+++ DependencyInjection/RpcServerExtension.php
@@ -14,26 +14,31 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class RpcServerExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $config        = $this->processConfiguration($configuration, $configs);
+	/**
+	 * {@inheritdoc}
+	 */
+	public function load(array $configs, ContainerBuilder $container)
+	{
+		$configuration = new Configuration();
+		$config        = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+		$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+		$loader->load('services.yml');
 
-        // paths
-        $paths = [];
-        foreach ($config['paths'] as $path) {
-            $paths[$path['namespace']] = $path['path'];
-        }
+		// paths
+		$paths = [];
+		foreach ($config['paths'] as $path) {
+			$paths[$path['namespace']] = $path['path'];
+		}
 
-        $container->setParameter('rpc.server.type', $config['type']);
-        $container->setParameter('rpc.server.paths', $paths);
-        $container->setParameter('rpc.server.proxy', $config['proxy']);
-        $container->setParameter('rpc.server.proxy.enable', $config['proxy']['enable']);
-    }
+		// methods
+		$methods = [];
+		foreach ($config['methods'] as $method) {
+			$methods[$method['name']] = $method['class'];
+		}
+
+		$container->setParameter('rpc.server.paths', $paths);
+		$container->setParameter('rpc.server.methods', $methods);
+		$container->setParameter('rpc.server.proxy', $config['proxy']);
+	}
 }
